51777b80eb885f15788252ff33138b4c8ad56c1f
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-06-07 14:11:57 +0900

    ローカルストレージAPIを使用

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// ローカルストレージ API
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+// Vue.js
+const app = new Vue ({
+  el: '#app',
+  data: {
+    message: 'こんにちは、Vue!'
+  }
+})
